9ab19885fffa41a75dca2d422a99b961bf7edd6d
kerry-okpere <kerryokpere@gmail.com>
2021-06-02 05:31:49 +0100

    pass data through route

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div @click="navigate">
     <img :src="img" :alt="name">
   </div>
 </template>
 <script>
+import { useRouter } from 'vue-router'
+
 export default {
   props: {
+    info: {
+      type: Object,
+      required: true
+    },
     img: {
       type: String,
       required: true,
@@ -14,8 +20,21 @@ export default {
       type: String,
     }
   },
-  setup() {
-    
+  setup(props) {
+    const router = useRouter()
+
+    const navigate = () => {
+      let query = {
+        breed: props.info.breed,
+      }
+      if(props.info.subBreed){
+        query.subBreed = props.info.subBreed
+      }
+      router.push({ name: 'dog', params: {id: props.info.id}, query: query })
+    }
+    return{
+      navigate
+    }
   },
 }
 </script>
