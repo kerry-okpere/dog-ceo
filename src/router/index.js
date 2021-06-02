f7667d9184b31fb2586c23657da48ec6f0c985b1
kerry-okpere <kerryokpere@gmail.com>
2021-06-02 05:32:06 +0100

    add route slug

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/views/Home.vue'
+import Dog from '/src/views/Dog.vue';
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
   },
+  { 
+    path: '/dog/:id', 
+    name: 'dog',
+    component: Dog 
+  }
+  // will match everything and put it under `$route.params.pathMatch`
+  // { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  // will match anything starting with `/user-` and put it under `$route.params.afterUser`
+  // { path: '/user-:afterUser(.*)', component: UserGeneric },
 ]
 const router = createRouter({
   history: createWebHistory(),
