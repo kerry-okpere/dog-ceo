3e8330a2e4c0c9e12ee797ba7447f6b226129fa6
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 16:48:06 +0100

    make card dynamic

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <figure @click="navigate" class="card">
+  <figure @click="navigate" :class="['card', size === 'small' ? 'card__small': 'card__medium']">
     <div :class="['card__content', {'animate-pulse': !imgSrc}]">
       <img class="card__content-img" :src="imgSrc" :alt="name" loading="lazy" ref="image" height="100" width="1000">
     </div>
     <figcaption class="card__title">
-      <p>{{info.breed}}</p>
+      <p :class="isClickable ? 'text-blue-500': 'text-gray-600'">
+        <span v-if="info.breed">{{info.breed}}</span>
+      </p>
     </figcaption>
   </figure>
 </template>
@@ -24,9 +26,21 @@ export default {
     },
     name:{
       type: String,
+    },
+    isClickable: {
+      type: Boolean,
+      default: true
+    },
+    size: {
+      type: String,
+      default: 'medium',
+      validator: function(value) {
+        return ['small', 'medium'].indexOf(value) !== -1
+      }
     }
   },
-  setup(props) {
+  emits: ['click'],
+  setup(props, { emit }) {
     // data
     const router = useRouter()
     const image = ref(null)
@@ -37,12 +51,11 @@ export default {
 
     // hooks
     onMounted(() => {
-      console.log('mounted')
       const observer = new IntersectionObserver(intersectionCallback)
       observer.observe(image.value)
     })
     onUnmounted(() => {
-      console.log('unmounted')
+      
     })
 
     // methods
@@ -51,18 +64,20 @@ export default {
 
       if (lazyImg.isIntersecting) {
         intersected.value = true
-        // console.log('i am in view')
       }
     }
 
     const navigate = () => {
-      let query = {
-        breed: props.info.breed,
+      if(props.isClickable){
+        let query = {
+          breed: props.info.breed,
+        }
+        if(props.info.subBreed){
+          query.subBreed = props.info.subBreed
+        }
+        router.push({ name: 'dog', params: {id: props.info.id}, query: query })
       }
-      if(props.info.subBreed){
-        query.subBreed = props.info.subBreed
-      }
-      router.push({ name: 'dog', params: {id: props.info.id}, query: query })
+      emit('click')
     }
 
     return{
