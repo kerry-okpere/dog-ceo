19d8952305def23346548b5dc960f25bc3c7bbcd
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 16:48:16 +0100

    add signature

---
+++ src/components/Signature/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="px-4 py-2 my-4 bg-blue-50 bg-opacity-50 rounded-md shadow-lg inline-block text-xs">
+    <p>Made by <span class="text-blue-500">Kerry</span> with 💚</p>
+  </div>
+</template>
