721a63fb8c5842d9aa61a5c830fa6b2c55947d58
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:41:38 +0100

    style search component

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <input type="search" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"/>
+  <span class="inline-flex border border-gray-200 rounded-sm h-12 space-x-2 items-center px-3">
+    <input type="search" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+    :placeholder="placeholder"
+    class="h-full outline-none"/>
+    <search-icon class="h-5 w-5 text-gray-400"/>
+  </span>
 </template>
 <script>
+import { SearchIcon } from '@heroicons/vue/outline'
 
 export default {
+  components: { SearchIcon },
   props: {
     modelValue: {
       type: String, 
       required: true
+    },
+    placeholder: {
+      type: String, 
+      default: 'Search Dogs'
     }
   },
   emits: ['update:modelValue']
