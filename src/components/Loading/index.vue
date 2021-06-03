07d05558ca6f459b11edc22d7d553c444f4e2d64
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:40:47 +0100

    create loading component

--- src/components/Loading/index.vue
+++ src/components/Loading/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    Loading...
+  <div class="my-16 text-center">
+    <div class="my-4 mx-auto animate-zoom h-32 w-32 bg-blue-500 bg-opacity-50 rounded-full flex items-center justify-center">
+      <span class="text-7xl">🐶</span>
+    </div>
+    <p><slot>Loading...</slot></p>
   </div>
 </template>
 <script>
