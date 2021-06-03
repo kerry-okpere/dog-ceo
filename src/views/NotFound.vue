ddb6feed8410e20c9cfb8aebfdb5af612177b213
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 16:49:13 +0100

    create 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <empty />
+    <h1 class="text-3xl text-blue-500 mt-6">404</h1>
+    <p>Page not found.</p>
+  </div>
+  
+</template>
+<script>
+import Empty from "/src/components/Icons/dog-house.vue";
+
+export default {
+  components: { Empty }
+}
+</script>
