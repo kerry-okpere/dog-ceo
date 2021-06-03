9450d4c83851f7d66120c69cd50c238c1e7505a8
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:42:26 +0100

    style select component

--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -1,7 +1,7 @@
 <template>
-<fieldset>
-  <legend>{{title}}</legend>
-  <select :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" id="">
+<fieldset class="border relative inline-block w-48 px-3 pb-2 pt-1 rounded-sm">
+  <legend class="mx-3 text-xs text-gray-400">{{title}}</legend>
+  <select class="h-full w-full outline-none" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
     <option disabled v-if="placeholder" value="null" >{{placeholder}}</option>
     <option v-for="(option, i) in options" :key="i" :value="option"  @click="$emit('select', option)">
       {{option}}
@@ -26,9 +26,6 @@ export default {
       type: String,
       required: true
     }
-  },
-  setup() {
-  
   },
 }
 </script>
