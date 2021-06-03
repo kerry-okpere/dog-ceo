51fd1c90276ac5163467adfcc83514484c20d157
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:27:16 +0100

    add: dynamic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        zoom: 'zoom 2s infinite'
+      },
+      keyframes: {
+        zoom: {
+          '0%, 100%': {
+            transform: 'scale(1.2)'
+          },
+          '50%': {
+            transform: 'scale(0.8)'
+          },
+        }
+      },
+      gridTemplateColumns: {
+       'body': 'repeat(auto-fit, minmax(250px, 1fr))',
+      }
+    },
   },
   variants: {
     extend: {},
