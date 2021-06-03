76bbd83ba9bc4f6b25daab628261d95471686665
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:31:30 +0100

    remove unused css file

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path');
 const pathSrc = path.resolve(__dirname, "./src");
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "${pathSrc}/assets/style/main.scss";`
+        // additionalData: `@import "${pathSrc}/assets/style/main.scss";`
       }
     },
   },
