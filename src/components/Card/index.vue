d783b5e074a9928400b3100d59e29d05be29fe91
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:43:38 +0100

    style card component

--- src/components/Card/index.vue
+++ src/components/Card/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <div @click="navigate">
-    <img :src="img" :alt="name">
-  </div>
+  <figure @click="navigate" class="card">
+    <div :class="['card__content', {'animate-pulse': !imgSrc}]">
+      <img class="card__content-img" :src="imgSrc" :alt="name" loading="lazy" ref="image" height="100" width="1000">
+    </div>
+    <figcaption class="card__title">
+      <p>{{info.breed}}</p>
+    </figcaption>
+  </figure>
 </template>
 <script>
 import { useRouter } from 'vue-router'
+import { ref, onMounted, computed, onUnmounted } from "vue";
 
 export default {
   props: {
@@ -21,7 +27,33 @@ export default {
     }
   },
   setup(props) {
+    // data
     const router = useRouter()
+    const image = ref(null)
+    const intersected = ref(false)
+    
+    // computed
+    const imgSrc = computed(() => intersected.value ? props.img : '')
+
+    // hooks
+    onMounted(() => {
+      console.log('mounted')
+      const observer = new IntersectionObserver(intersectionCallback)
+      observer.observe(image.value)
+    })
+    onUnmounted(() => {
+      console.log('unmounted')
+    })
+
+    // methods
+    const intersectionCallback = entries => {
+      const lazyImg = entries[0]
+
+      if (lazyImg.isIntersecting) {
+        intersected.value = true
+        // console.log('i am in view')
+      }
+    }
 
     const navigate = () => {
       let query = {
@@ -32,9 +64,15 @@ export default {
       }
       router.push({ name: 'dog', params: {id: props.info.id}, query: query })
     }
+
     return{
-      navigate
+      navigate,
+      image, 
+      imgSrc
     }
   },
 }
 </script>
+<style lang="scss" scoped>
+@import './_index.scss';
+</style>
