a8e27e70d6384fbe0b518e8e8cef633db10849ae
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:27:28 +0100

    setup scss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path');
+const pathSrc = path.resolve(__dirname, "./src");
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${pathSrc}/assets/style/main.scss";`
+      }
+    },
+  },
+  plugins: [
+    vue()
+  ]
 })
