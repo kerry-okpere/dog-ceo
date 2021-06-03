8e214a7421d8d7232445766fd1128efa1e113330
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:33:55 +0100

    create alert component

--- src/components/Alert/index.vue
+++ src/components/Alert/index.vue
@@ -1,14 +1,53 @@
 <template>
-  <span>
-    {{title}}
-  </span>
+  <div :class="['flex py-4 px-12 justify-center space-x-6 items-center max-w-7xl rounded-sm', bgColor]">
+    <span :class="['h-6 w-16 sm:w-12 md:w-6 self-center',iconColor]">
+      <slot>
+        <success-icon v-if="variant === 'success'"/>
+        <err-icon v-else-if="variant === 'error'"/>
+        <info-icon v-else/>
+      </slot>
+    </span>
+    <p class="text-sm">{{title}}</p>
+  </div>
 </template>
 <script>
+import { computed, ref } from "vue";
+import { InformationCircleIcon } from '@heroicons/vue/outline'
+import { ExclamationCircleIcon } from '@heroicons/vue/outline'
+import { checkCircleIcon } from '@heroicons/vue/outline'
+
 export default {
+  components: { 
+    'info-icon': InformationCircleIcon,
+    'err-icon': ExclamationCircleIcon,
+    'success-icon': checkCircleIcon
+  },
   props: {
     title: {
       type: String,
       reauired: true
+    },
+   variant: {
+    type: String,
+    default: 'warning',
+    validator: function(value) {
+        return ['success', 'warning', 'error'].indexOf(value) !== -1
+      }
+    }
+  },
+  setup(props){
+    const color = computed(() => {
+      if(props.variant === 'error') return 'red'
+      if(props.variant === 'success') return 'green'
+      return 'yellow'
+    })
+    const iconColor = ref(`text-${color.value}-600`)
+    const bgColor = ref(`bg-${color.value}-100`)
+
+    console.log(iconColor.value, )
+    return{
+      iconColor,
+      bgColor
     }
   }
 }
