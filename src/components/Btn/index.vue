857f55a168bfb0e2a04a7bb07c977556d3a320fa
kerry-okpere <kerryokpere@gmail.com>
2021-06-03 10:38:40 +0100

    create button component

---
+++ src/components/Btn/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="rounded-sm bg-blue-600 hover:bg-blue-700 transition-colors py-3 px-4 text-white"
+    @click="$emit('click')">
+    <slot>
+      Button
+    </slot>
+  </button>
+</template>
+<script>
+
+export default {
+  emits: ['click'],
+}
+</script>
