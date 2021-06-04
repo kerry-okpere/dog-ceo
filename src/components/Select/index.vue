cc57abcdfa747cd34600566fc85a547f6aa802b8
kerry-okpere <kerryokpere@gmail.com>
2021-06-04 12:00:33 +0100

    remove unused code

--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -2,7 +2,7 @@
   <fieldset class="border relative inline-block w-48 px-3 pb-2 pt-1 rounded-sm">
     <legend class="mx-3 text-xs text-gray-400">{{title}}</legend>
     <select class="h-full w-full outline-none" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
-      <option disabled v-if="placeholder" value="null" >{{placeholder}}</option>
+      <option v-if="placeholder" value="null" >{{placeholder}}</option>
       <option v-for="(option, i) in options" :key="i" :value="option"  @click="$emit('select', option)">
         {{option}}
       </option>
